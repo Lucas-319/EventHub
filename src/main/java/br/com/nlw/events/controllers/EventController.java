c673dc34f698d0d5233a5f94fb1cf7d41259ed87
Lucas Daniel <kingfoxtroi@gmail.com>
2025-04-11 00:31:11 -0300

    fix: corrigido endpoints /events

--- src/main/java/br/com/nlw/events/controllers/EventController.java
+++ src/main/java/br/com/nlw/events/controllers/EventController.java
@@ -11,13 +11,14 @@ import org.springframework.web.util.UriComponentsBuilder;
 import java.util.List;
 
 @RestController
+@RequestMapping("/events")
 public class EventController {
 
 	@Autowired 
 	private EventService eventService;
 
 	@Operation(summary = "Cadastra um novo evento", method = "POST")
-	@PostMapping("/events")
+	@PostMapping
 	public ResponseEntity<EventDto> addNewEvent(@RequestBody EventDto newEvent,
 												UriComponentsBuilder uriComponentsBuilder){
 		EventDto eventResponse = eventService.addNewEvent(newEvent);
@@ -28,13 +29,13 @@ public class EventController {
 	}
 
 	@Operation(summary = "Lista todos os eventos", method = "GET")
-	@GetMapping("/events")
+	@GetMapping
 	public ResponseEntity<List<EventDto>> getEvents(){
 		return ResponseEntity.status(200).body(eventService.findAllEvents());
 	}
 
 	@Operation(summary = "Lista um evento específico pelo prettyName do evento", method = "GET")
-	@GetMapping("/events/{prettyName}")
+	@GetMapping("/name/{prettyName}")
 	public ResponseEntity<EventDto> getEventByPrettyName(@PathVariable String prettyName){
 		var event = eventService.findEventByPrettyName(prettyName);
 		
@@ -45,8 +46,8 @@ public class EventController {
 	}
 
 	@Operation(summary = "Lista um evento específico pelo ID", method = "GET")
-	@GetMapping("/events/specific")
-	public ResponseEntity<EventDto> getEventById(@RequestParam Integer id){
+	@GetMapping("/{id}")
+	public ResponseEntity<EventDto> getEventById(@PathVariable Integer id){
 		var event = eventService.findEventById(id);
 		
 		if(event != null) {
